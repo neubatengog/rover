197e24fb705e16b7cb2268a116c715024927a704
neubaten <neubaten@gmail.com>
2014-08-18 20:50:01 -0400

    se agrega cuadro de informacion

--- consola_rover.py
+++ consola_rover.py
@@ -3,57 +3,56 @@ import glob
 from Tkinter import *
 
 
-""" Funcion que scanea las rutas fisicas de los puertos seriales """
-def scan():
-    return glob.glob('/dev/tty.*') + glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*')
 
 class App:
 	def __init__(self, master):
 		frame = Frame (master)
 		frame.pack()
 		"""____________movimiento de camara, j, k , l , p____ """
-		Label(frame, text = 'CAMARA'). grid(row=0, column=1)
+		Label(frame, text = 'CAMARA',font = "Helvetica 12 bold",fg = "blue",
+		 bg = "yellow"). grid(row=0, column=1)
 		
 		#boton de centrado de camara
-		btnArriba = Button(frame, text='^', command=self.arriba)
+		btnArriba = Button(frame, text='^', width=6, height=2, command=self.arriba)
 		btnArriba.grid(row=1, column=1)
 		
 		#boton de movimiento izq de camara
-		btnIzq = Button(frame, text='<<', command=self.izq)
+		btnIzq = Button(frame, text='<<', width=6, height=2,command=self.izq)
 		btnIzq.grid(row=2, column=0)
 
 		#boton de movimiento de paneo 180 y centrado de camara
-		btnPan = Button(frame, text='PANEO', command=self.pan)
+		btnPan = Button(frame, text='PANEO', width=6, height=2,command=self.pan)
 		btnPan.grid(row=2, column=1)
 		
 		#boton de movimiento derecha de camara
-		butDer = Button(frame, text='>>', command=self.der)
+		butDer = Button(frame, text='>>',width=6, height=2, command=self.der)
 		butDer.grid(row=2, column=2)		
 		"""_______________ fin movimiento__________________ """
 
 		"""_______________ movimiento rover__________________"""
-		Label(frame, text = 'ROVER'). grid(row=6, column=1)
+		Label(frame, text = 'ROVER', font = "Helvetica 12 bold",fg = "blue",
+		 bg = "yellow"). grid(row=6, column=1)
 		
 		#boton de movimiento avanzar
-		btnAvan = Button(frame, text='^', command=self.avanzar)
+		btnAvan = Button(frame, text='^',width=7, height=3, command=self.avanzar)
 		btnAvan.grid(row=7, column=1)
 		#boton de movimiento izquierda
-		btnIz = Button(frame, text='<<', command=self.moveiz)
+		btnIz = Button(frame, text='<<',width=7, height=3, command=self.moveiz)
 		btnIz.grid(row=8, column=0)
 		#boton de movimiento derecha
-		butDe = Button(frame, text='>>', command=self.movede)
+		butDe = Button(frame, text='>>',  width=7, height=3, command=self.movede)
 		butDe.grid(row=8, column=2)
 		#boton de movimiento detener
-		butStop = Button(frame, text='S', command=self.moveStop)
+		butStop = Button(frame, text='S',width=4, height=3, command=self.moveStop)
 		butStop.grid(row=8, column=1)
 		#boton de movimiento retroceder
-		butRetro = Button(frame, text='V', command=self.retro)
+		butRetro = Button(frame, text='V',width=7, height=3, command=self.retro)
 		butRetro.grid(row=9, column=1)
 		#boton de aumentar velocidad
-		butAumentar = Button(frame, text='+', command=self.Aumentar)
+		butAumentar = Button(frame, text='+',width=3, height=3, command=self.Aumentar)
 		butAumentar.grid(row=7, column=3)
 		#boton de disminuir velocidad
-		butDisminuir = Button(frame, text='-', command=self.Disminuir)
+		butDisminuir = Button(frame, text='-',width=3, height=3, command=self.Disminuir)
 		butDisminuir.grid(row=9, column=3)
 		"""_______________ fin movimiento rover__________________"""
 		
@@ -61,7 +60,7 @@ class App:
 		Label(frame, text = 'Puertos detectados:'). grid(row=13, column=0)
 		listbox = Listbox(frame, height = 2, selectmode=SINGLE)
 		listbox.bind("<<ListboxSelect>>",self.Conectar)
-		for puerto in scan():
+		for puerto in self.scan():
 			listbox.insert(END, puerto)
 		listbox.grid(row=13, column=1)
 
@@ -130,12 +129,17 @@ class App:
 		self.accion.set("VEL--")
 		ser.write('-\n')
 
+	""" Funcion que scanea las rutas fisicas de los puertos seriales """
+	def scan(self):
+		return glob.glob('/dev/tty.*') + glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*')
+
+
 if __name__=='__main__':	
 	principal = Tk()
 	#titulo de la ventana
 	principal.wm_title('ROVER')
 	#tamano de la ventana principal
-	principal.geometry("400x400")
+	principal.geometry("450x350")
 	app = App(principal)
 	principal.mainloop()
 	
